0da5f84a7205bf69cf9371dcdbdc214edf3db895
abhishek <developer.abhishekmca@gmail.com>
2025-08-03 10:37:20 +0530

    Created Sign Up & Contact Page and front end completed

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -3,25 +3,27 @@
 
 import { useState } from 'react';
 import { useLoginModal } from "../../context/LoginModalContext";
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 export default function LoginModal() {
   const { isOpen, closeModal } = useLoginModal();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log('Logging in with:', { email, password });
-    closeModal();
-  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bordered shadow-sm bg-black bg-opacity-50 z-50">
       <div className="bg-white rounded-lg p-6 w-full max-w-md shadow-lg">
-        <h2 className="text-xl font-semibold mb-4">Login</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
+        <button className='btn btn-sm btn-circle btn-ghost hover:bg-red-400 hover:text-white relative float-right bg-black text-white right-2 top-1 text-white' onClick={closeModal}>X</button>
+        <h2 className="text-xl font-bold mb-4">Login</h2>
+        <form onSubmit={handleSubmit((data) => console.log(data))} className="space-y-4">
           <div>
             <label className="block text-sm font-medium mb-1" htmlFor="email">
               Email
@@ -30,10 +32,10 @@ export default function LoginModal() {
               type="email"
               id="email"
               className="w-full border border-gray-300 rounded px-3 py-2"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
+              {...register('email', { required: true })}
+              placeholder='Enter your email'
             />
+            {errors.email && <p  className='error text-red-500'>Email is required.</p>}
           </div>
           <div>
             <label className="block text-sm font-medium mb-1" htmlFor="password">
@@ -43,11 +45,14 @@ export default function LoginModal() {
               type="password"
               id="password"
               className="w-full border border-gray-300 rounded px-3 py-2"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
+              {...register('password', { required: true })}
+              placeholder='Enter your password'
             />
+            {errors.password && <p className='error text-red-500'>Password is required.</p>}
           </div>
+           <div className="not-registered flex justify-start gap-2">
+              <h2>Not Registered ?  </h2><Link href="/signup" className='text-red-600 hover:text-red-800' onClick={closeModal}> SignUp! </Link>
+            </div>
           <div className="flex justify-end gap-2">
             <button
               type="button"
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 const contact = () => {
+    const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+
   return (
-    <div>
-      <h1>Contact Page</h1>
+    <div className="flex bg-zinc-200  p-8 bg-white max-w-xl m-auto mt-8 items-center justify-center  shadow-sm bg-black bg-opacity-50 z-50">
+      <div className="p-6 bg-white bordered w-full max-w-md shadow-md">
+        <Link href="/" className='btn btn-sm btn-circle btn-ghost hover:bg-red-400 hover:text-white relative float-right bg-black text-white right-2 top-1 text-white'>X</Link>
+        <h2 className="text-xl font-bold mb-4">Contact Us</h2>
+        <form onSubmit={handleSubmit((data) => console.log(data))} className="space-y-4">
+        <div>
+            <label className="block text-sm font-medium mb-1" htmlFor="fullName">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="w-full border border-gray-300 rounded px-3 py-2"
+             {...register('name', { required: true })}
+              placeholder='Enter your name'
+            />
+            {errors.name && <p  className='error text-red-500'>Name is required.</p>}
+          </div>
+          <div>
+            <label className="block text-sm font-medium mb-1" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="w-full border border-gray-300 rounded px-3 py-2"
+              {...register('email', { required: false })}
+              placeholder='Enter your email'
+            />
+            {errors.email && <p  className='error text-red-500'>Email is optional.</p>}
+          </div>
+          <div>
+            <label className="block text-sm font-medium mb-1" htmlFor="password">
+              Message
+            </label>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Type your message </legend>
+              <textarea className="textarea h-24" placeholder="Message"></textarea>
+              <div className="label">Optional</div>
+            </fieldset>
+          </div>
+          <div className="flex justify-start gap-2">
+            <button type="submit" className="px-6 py-2 cursor-pointer hover:bg-red-400 hover:text-white bg-blue-600 text-white rounded">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
